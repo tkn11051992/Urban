f339bbbb1d444ffeee87f36a6c78544f5aa97514
tkn11051992 <tkn11051992@gmail.com>
2024-11-24 19:23:13 +0300

    Delete variables.py

--- variables.py
+++
@@ -1,5 +0,0 @@
-number_of_completed_HW = 12
-number_of_hours_spent = 1.5
-course_name = 'Python'
-time_for_one_lesson = number_of_hours_spent / number_of_completed_HW
-print(f'Курс: {course_name}, всего задач: {number_of_completed_HW}, затрачено часов: {number_of_hours_spent}, среднее время выполнения {time_for_one_lesson}.')
