c3cbdcd27ac4c17e40da71a7e172d47528d28679
tkn11051992 <tkn11051992@gmail.com>
2025-01-12 21:44:32 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,6 @@
-immutable_var = ([1, "арбуз"])
+immutable_var = 1, "арбуз"
 print(immutable_var)
+#immutable_var[0] = 2 (нельзя изменить значения элементов кортежа, так как кортеж не поддерживает обращение по элементам)
 mutable_list = ([2, "дыня"])
 mutable_list[1] = "банан"
 print(mutable_list)
